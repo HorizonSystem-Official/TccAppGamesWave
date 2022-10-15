5b889d0b621cbd556b1157511f1a09c0ef410c6e
LucasBara-dising <lucas.Barauna098@gmail.com>
2022-10-15 13:23:45 -0300

    tela Login, splach, cadstro de cliente e recuperar senha com visual pronto

--- app/src/main/java/com/example/tccappgameswave/login.java
+++ app/src/main/java/com/example/tccappgameswave/login.java
@@ -2,7 +2,10 @@ package com.example.tccappgameswave;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
 
@@ -10,5 +13,31 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView txtEsqueceu = (TextView) findViewById(R.id.txtEsqueceu);
+        txtEsqueceu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TelaMudaSenha();
+            }
+        });
+
+        TextView TxtCriarConta = (TextView) findViewById(R.id.TxtCriarConta);
+        TxtCriarConta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TelaCriaConta();
+            }
+        });
+    }
+
+    public  void TelaMudaSenha(){
+        Intent MudaSenha = new Intent(getApplicationContext(), esqueceuAsenha.class);
+        startActivity(MudaSenha);
+    }
+
+    public  void TelaCriaConta(){
+        Intent CriaConta = new Intent(getApplicationContext(),cadastroCliente.class);
+        startActivity(CriaConta);
     }
 }
