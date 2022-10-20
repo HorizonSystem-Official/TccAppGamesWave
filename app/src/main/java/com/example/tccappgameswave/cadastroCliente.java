00a000d3d5a766d01d47991c50290ed5a78fb4c3
BrnBastos <bruninhosbastos0228@gmail.com>
2022-10-20 11:54:35 -0300

    Butons voltar

--- app/src/main/java/com/example/tccappgameswave/cadastroCliente.java
+++ app/src/main/java/com/example/tccappgameswave/cadastroCliente.java
@@ -2,7 +2,11 @@ package com.example.tccappgameswave;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class cadastroCliente extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class cadastroCliente extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_cliente);
+
+        ImageView imgVoltar = (ImageView) findViewById(R.id.imageViewVoltar);
+        imgVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TelaLogin();
+            }
+        });
     }
+
+    public  void TelaLogin(){
+        Intent Login = new Intent(getApplicationContext(), login.class);
+        startActivity(Login);
+    }
+
 }
